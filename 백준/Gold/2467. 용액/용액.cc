867e8a2a4191645737318a5e7fb85ff54e6c06c2
Jimin Lee <87521781+easyminnn@users.noreply.github.com>
2023-08-29 17:25:56 +0900

    [Gold V] Title: 용액, Time: 68 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -12,19 +12,7 @@ int main(){
         cin >> liquid[i];
     }
 
-    int index = lower_bound(liquid, liquid+N, 0) - liquid;
-
-    if (index == 0 || index >= N){
-        if (index ==0){
-            cout << liquid[0] << " " << liquid[1] << "\n";
-            return 0;
-        }
-        else{
-            cout << liquid[N-2] << " " << liquid[N-1] << "\n";
-        }
-    }
     
-    else{
         int l = 0;
         int r = N-1;
         int min_r, min_l;
@@ -43,10 +31,6 @@ int main(){
 
         cout << liquid[min_l] << " " << liquid[min_r] << "\n";
         return 0;
-    }
-
-
-
-
+    
 
 }
